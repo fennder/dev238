88efa792af5bba00c84a6e7ede91181bb78c2ed7
Edir P Santos <fennder@gmail.com>
2024-04-30 11:55:39 -0300

    Create func

--- src/br/tec/abrindoportas/principal/Principal.java
+++ src/br/tec/abrindoportas/principal/Principal.java
@@ -1,44 +1,20 @@
 package br.tec.abrindoportas.principal;
 
-import java.util.Scanner;
+import br.tec.abrindoportas.funcao.Funcoes;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		//BLOCO - Declaração de variável
-		int nota1, nota2, nota3, nota4, notar;
-		float media, mediar;
-		Scanner read = new Scanner(System.in);
-			
-		//BLOCO - Entrada de dados
-		System.out.println("Digite a nota 1: ");
-		nota1 = read.nextInt();
-		System.out.println("Digite a nota 2: ");
-		nota2 = read.nextInt();
-		System.out.println("Digite a nota 3: ");
-		nota3 = read.nextInt();
-		System.out.println("Digite a nota 4: ");
-		nota4 = read.nextInt();
-				
-		//BLOCO - Processamento
-		media = (nota1+nota2+nota3+nota4)/4;
+		//BLOCO declaração de variáveis
+		Funcoes func = new Funcoes();
 		
-		//BLOCO - Saida de dados
-		if (media >= 7) {//verdadeiro
-			System.out.println("Aluno aprovado, com média: " + media);
-			
-		}else {
-			System.out.println("Digite a nota da recuperação: ");
-			notar = read.nextInt();
-			mediar = (media+notar)/2;
-			
-			if (mediar >= 5) {
-				System.out.println("Aluno aprovado com média em recuperação: " + mediar);
-			}else {
-				System.out.println("Aluno reprovado, com média: " + mediar);
-			}
-		}
+		//BLOCO Entrada de dados
+//		func.mediaAritmetica();
+		func.sequenciaS();
+		
+		//S = 1, 4, 7, 10, 13, 16, 19, 21, 24, 27
+		//    0   1  2  3  4    5   6  7   8   9   100    1000   10000
 
-	}//fechamento method main
-}//fechamento da classe
+	}
+}
 
