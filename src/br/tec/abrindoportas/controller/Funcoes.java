36522e7665d605918287800b9d0a209bbfa775ee
Edir P Santos <fennder@gmail.com>
2024-05-13 09:59:23 -0300

    Create fucn escolha

--- src/br/tec/abrindoportas/controller/Funcoes.java
+++ src/br/tec/abrindoportas/controller/Funcoes.java
@@ -3,7 +3,7 @@ package br.tec.abrindoportas.controller;
 import java.util.Scanner;
 
 public class Funcoes {
-	
+
 	public void mediaAritmetica() {
 		//BLOCO - Declaração de variável
 		int nota1, nota2, nota3, nota4, notar;
@@ -39,7 +39,7 @@ public class Funcoes {
 			}
 		}
 	}
-	
+
 	/*
 	 * Criar um programa que recebe um numero e verifica se
 	 * ele está entre 0 e 10 e se 
@@ -49,17 +49,17 @@ public class Funcoes {
 		//variável
 		int numero;
 		Scanner read = new Scanner(System.in);
-		
+
 		//entrada dados
 		System.out.println("Digite o número: ");
 		numero = read.nextInt();
-		
+
 		//processamento e saída dados
 		if((numero >= 0) && (numero <= 10)) {
 			if(numero % 2 == 0) {
 				System.out.println("O numero " 
-					+ numero 
-					+ " está entre a faixa de 0 a 10 e é par");
+						+ numero 
+						+ " está entre a faixa de 0 a 10 e é par");
 			}else {
 				System.out.println("O numero " 
 						+ numero 
@@ -70,6 +70,59 @@ public class Funcoes {
 					+ numero 
 					+ " não está entre a faixa de 0 a 10.");
 		}
-		
+
 	}
+
+	//Função irá ler um inteiro e mostrar o dia da semana
+	public void escolha() {
+		int dayWeekend = 6;
+		Scanner read = new Scanner(System.in);
+		
+//		System.out.println("Digite o dia da semana:");
+//		System.out.println("1 = Domingo,\n 2 = Segunda,\n 0 = Sair");
+//		dayWeekend = read.nextInt();
+		
+		while(dayWeekend != 0) {
+			System.out.println("Dentro do enquanto");
+			System.out.println("Digite o dia da semana:");
+			System.out.println("1 = Domingo,\n2 = Segunda,\n0 = Sair");
+			dayWeekend = read.nextInt();
+			
+			//Esta estrutura serve para escolher uma opção. Retorna dia da semana.
+			switch (dayWeekend) {
+				case 1: {
+					System.out.println("Domingo");
+					break;
+				}
+				case 2: {
+					System.out.println("Segunda-feira");
+					break;
+				}
+				case 3: {
+					System.out.println("Terça-feira");
+					break;
+				}
+				case 4: {
+					System.out.println("Quarta-feira");
+					break;
+				}
+				case 5: {
+					System.out.println("Quinta-feira");
+					break;
+				}
+				case 6: {
+					System.out.println("Sexta-feira");
+					break;
+				}
+				case 7: {
+					System.out.println("Sábado");
+					break;
+				}
+				default: {
+					System.out.println("\nOpção escolhida é existente.");
+				}
+			
+			}
+		}
+	} //Função escolha
 }
--- src/br/tec/abrindoportas/principal/Principal.java
+++ src/br/tec/abrindoportas/principal/Principal.java
@@ -1,18 +1,20 @@
 package br.tec.abrindoportas.principal;
 
-import java.util.Scanner;
-
 import br.tec.abrindoportas.controller.Funcoes;
 import br.tec.abrindoportas.controller.VideoLocadora;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		//Instância da classe Funções
 		Funcoes func = new Funcoes();
-//		func.mediaAritmetica();
-		func.verificaPar();
+		//func.mediaAritmetica();
+//		func.verificaPar();
+		func.escolha();
+		
+		//Instância da classe VideoLocadora
 		VideoLocadora video = new VideoLocadora();
-//		video.locar();
+		//video.locar();
 				
-	} // fim method main
-} //fim class Principal
+	}
+}
